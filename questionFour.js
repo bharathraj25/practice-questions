353b33eb86a94371a3016d2acce65f066a8e5bb6
Bharath Raj <BharathRajgngowdara@gmail.com>
2023-01-15 00:09:17 +0530

    feat: add arrow function - getLongestStringArrow

--- questionFour.js
+++ questionFour.js
@@ -11,5 +11,18 @@ function getLongestStringRegular(sArray) {
     return longestString
 }
 
+// Arrow function
+const getLongestStringArrow = sArray => {
+    let longestString = ""
+    sArray.forEach(string => {
+        if(string.length > longestString.length) {
+            longestString = string
+        }
+    });
+
+    return longestString
+}
+
 const sampleInput = ["we", "love", "code", "academy"]
 console.log(getLongestStringRegular(sampleInput))
+console.log(getLongestStringArrow(sampleInput))
