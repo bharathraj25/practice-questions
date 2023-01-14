59ff6c0265658431bb7d237fb1074c38e91fe93b
Bharath Raj <BharathRajgngowdara@gmail.com>
2023-01-15 00:14:55 +0530

    feat: add arrow function - checkIfLeapYearArrow

--- questionFive.js
+++ questionFive.js
@@ -7,5 +7,14 @@ function checkIfLeapYearRegular(year) {
     return false
 }
 
+// Arrow function
+const checkIfLeapYearArrow = year => {
+    if((year%4==0) && ((year%400==0) || (year%100!==0))) {
+        return true
+    }
+    return false
+}
+
 const sampleInput = 2000
 console.log(checkIfLeapYearRegular(sampleInput))
+console.log(checkIfLeapYearArrow(sampleInput))
