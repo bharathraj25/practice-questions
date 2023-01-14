a0f51ab024039daa450815fb7bc464bad7630975
Bharath Raj <BharathRajgngowdara@gmail.com>
2023-01-14 23:31:36 +0530

    feat: add getLargestEvenNumberArrow function

--- questionOne.js
+++ questionOne.js
@@ -8,5 +8,15 @@ function getLargestEvenNumberRegular(nArray) {
     return -1
 }
 
+// Arrow function
+const getLargestEvenNumberArrow = nArray => {
+    const largestNumber = Math.max(...nArray)
+    if(largestNumber % 2 === 0){
+        return largestNumber
+    }
+    return -1
+}
+
 const sampleInput = [1, 2, 3, 4, 5, 6, -3, -8, -11]
 console.log(getLargestEvenNumberRegular(sampleInput))
+console.log(getLargestEvenNumberArrow(sampleInput))
