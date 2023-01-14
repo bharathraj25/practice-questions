f4693afcd6f94a66f668fab87dae6030d6511105
Bharath Raj <BharathRajgngowdara@gmail.com>
2023-01-15 00:23:27 +0530

    feat: add arrow function - checkIfMatchPostfixArrow

--- questionSix.js
+++ questionSix.js
@@ -11,5 +11,15 @@ function checkIfMatchPostfixRegular(string) {
     return false
 }
 
+// Arrow function
+const checkIfMatchPostfixArrow = string => {
+    let stringLength = string.length
+    if(stringLength >= 6 && string.indexOf(postfixString) == stringLength - 6){
+        return true
+    }
+    return false
+}
+
 const sampleInput = "javaScript"
 console.log(checkIfMatchPostfixRegular(sampleInput))
+console.log(checkIfMatchPostfixArrow(sampleInput))
