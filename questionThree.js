34d89cf236f9e64526684b80778b989c0821aee9
Bharath Raj <BharathRajgngowdara@gmail.com>
2023-01-15 00:02:57 +0530

    feat: add arrow function - reverseCapitalizationArrow

--- questionThree.js
+++ questionThree.js
@@ -15,5 +15,22 @@ function reverseCapitalizationRegular(string) {
     return resString
 }
 
+// Arrow function
+const reverseCapitalizationArrow = string => {
+    let resString = ""
+    for (let c = 0; c < string.length; c++ ) {
+
+        // check if upperCase
+        if(string[c].toUpperCase() == string[c]){
+            resString += string[c].toLowerCase()
+        }
+        else {
+            resString += string[c].toUpperCase()
+        }
+    }
+    return resString
+}
+
 const sampleInput = "MeRRy hAD a LITTle lAMp"
 console.log(reverseCapitalizationRegular(sampleInput))
+console.log(reverseCapitalizationArrow(sampleInput))
