58fb86e2df5d8cfc4dd4422dc753a89c5dbfec1e
Bharath Raj <BharathRajgngowdara@gmail.com>
2023-01-15 10:00:32 +0530

    feat: add arrow function - replaceNextCharacterArrow

--- questionNine.js
+++ questionNine.js
@@ -15,5 +15,22 @@ function replaceNextCharacterRegular(string) {
     return resString
 }
 
+// Arrow function
+const replaceNextCharacterArrow = string => {
+    
+    let resString = "", nextLetter
+    for(let c = 0; c < string.length; c++) {
+        nextLetter = String.fromCharCode(1 + string[c].charCodeAt(0))
+
+        if(nextLetter == "{")
+            resString = resString.concat("a")
+        else
+            resString = resString.concat(nextLetter)
+    }
+
+    return resString
+}
+
 const sampleInput = "lazyinterns"
 console.log(replaceNextCharacterRegular(sampleInput))
+console.log(replaceNextCharacterArrow(sampleInput))
